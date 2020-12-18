32aacdbe45c04dfa26054a00b25a3043db1c804e
Maetad Sukarasud <maetad.s@siamraj.com>
2020-12-19 01:00:01 +0700

    ref: update main.go for retriving new data

--- main.go
+++ main.go
@@ -10,29 +10,43 @@ import (
 	"github.com/pakkaparn/taxcalculator/taxcal"
 )
 
+var reader *bufio.Reader
+
 func main() {
-	reader := bufio.NewReader(os.Stdin)
-	fmt.Println("Tax Calcualtor")
-	fmt.Println("--------------")
+	reader = bufio.NewReader(os.Stdin)
 
 	for {
-		fmt.Println("What is you annual income?")
-		fmt.Print("-> ")
-		text, _ := reader.ReadString('\n')
-		text = strings.Replace(text, "\n", "", -1)
+		fmt.Println("Tax Calcualtor")
+		fmt.Println("--------------")
 
-		income, err := strconv.ParseFloat(text, 2)
-
-		if err != nil {
-			fmt.Printf("Sorry, '%v' is not a number", text)
-			fmt.Println("")
-			continue
-		}
-
+		income := askForAnnualIncome()
 		tax := taxcal.Calculate(income)
 
 		fmt.Printf("You have to pay %.2f", tax)
 		fmt.Println("")
 		fmt.Println("--------------")
 	}
+}
+
+func askForAnnualIncome() float64 {
+	fmt.Println("What is you annual income?")
+	fmt.Print("-> ")
+
+	text := input()
+	income, err := strconv.ParseFloat(text, 2)
+
+	if err != nil {
+		fmt.Printf("Sorry, '%v' is not a number", text)
+		fmt.Println("")
+		return askForAnnualIncome()
+	}
+
+	return income
+}
+
+func input() string {
+	text, _ := reader.ReadString('\n')
+	text = strings.Replace(text, "\n", "", -1)
+
+	return text
 }
